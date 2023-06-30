a46e40d50948b3259855bcca292c8dead077fdeb
Daniel Quintana <quintana.danielarmando@gmail.com>
2023-06-30 17:00:54 +0100

    created material react table and adjust size logo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,49 +1,87 @@
-import {useState} from 'react';
-import logo from './assets/images/logo-universal.png';
-import './App.css';
-import {Greet} from "../wailsjs/go/main/App";
-import TableConfigMap from './components/Table';
-import { Box } from '@mui/material';
+import { useMemo, useState } from "react";
+import "./App.css";
+import { Greet } from "../wailsjs/go/main/App";
+import logo from "./assets/images/logo-universal.png";
 
+import { Box } from "@mui/material";
+import MaterialReactTable, { MRT_ColumnDef } from "material-react-table";
 
 interface Response {
-    Name: string,
-    NameSpace: string,
-    Value: string
-  }
-  
-
-function App() {
-    const [resultText, setResultText] = useState<Response[]>([]);
-    const [name, setName] = useState('');
-    const updateName = (e: any) => setName(e.target.value);
-    const updateResultText = (result: any) => {
-        let obj: Response[] = JSON.parse(result);
-        setResultText(obj);
-    };
-
-    function greet() {
-        Greet(name).then(updateResultText);
-    }
-
-    console.log("resultText: ", resultText, typeof(resultText))
-
-    // @ts-ignore
-    return (
-        <>
-        <div id="App">
-            <img src={logo} id="logo" alt="logo" width="50" height="60"/>
-            <div id="input" className="input-box">
-                <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text"/>
-                <button className="btn" onClick={greet}>Greet</button>
-            </div>
-        </div>
-        <br/>
-        <Box>
-            <TableConfigMap dataValue={resultText}/>
-        </Box>
-        </>
-    )
+  Name: string;
+  NameSpace: string;
+  Value: string;
 }
 
-export default App
+function App() {
+  const [resultText, setResultText] = useState<Response[]>([]);
+  const [name, setName] = useState("");
+  const updateName = (e: any) => setName(e.target.value);
+  const updateResultText = (result: any) => {
+    let obj: Response[] = JSON.parse(result);
+    setResultText(obj);
+  };
+
+  function greet() {
+    Greet(name).then(updateResultText);
+  }
+
+  console.log("resultText: ", resultText, typeof resultText);
+
+  const columnsData: MRT_ColumnDef<{
+    Name: string;
+    NameSpace: string;
+    Value: string;
+  }>[] = useMemo(
+    () => [
+      {
+        header: "Name",
+        accessorKey: "Name",
+      },
+      {
+        header: "NameSpace",
+        accessorKey: "NameSpace",
+      },
+      {
+        header: "Value",
+        accessorKey: "Value",
+      },
+    ],
+    []
+  );
+
+  // @ts-ignore
+  return (
+    <>
+      <Box justifyContent="center" alignItems="center">
+        <img src={logo} id="logo" alt="logo" />
+        <br />
+        <div id="input" className="input-box">
+          <input
+            id="name"
+            className="input"
+            onChange={updateName}
+            autoComplete="off"
+            name="input"
+            type="text"
+          />
+          <button className="btn" onClick={greet}>
+            Find
+          </button>
+        </div>
+      </Box>
+
+      <br />
+      {resultText.length > 0 ? (
+        <Box m="40px 0 0 0">
+            <Box>
+                <MaterialReactTable columns={columnsData} data={resultText} />
+            </Box>
+        </Box>
+      ) : (
+        <p>Not data exists</p>
+      )}
+    </>
+  );
+}
+
+export default App;
